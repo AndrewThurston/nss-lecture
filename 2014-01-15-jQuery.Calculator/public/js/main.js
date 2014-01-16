85a77502e9c324b8d35cf17e8b12b9b8d0a48235
Robert Fryman <robert.fryman@gmail.com>
2014-01-16 10:54:25 -0600

    add superCalc

--- 2014-01-15-jQuery.Calculator/public/js/main.js
+++ 2014-01-15-jQuery.Calculator/public/js/main.js
@@ -1,32 +1,63 @@
-$(document).ready(initialized);
+$(document).ready(initialize);
 
 function initialize(){
   $('#calc').click(calculate);
   $('#clear').click(clear);
+  $('#sum').click(sum);
+  $('#product').click(prod);
 }
-
+function clear(){
+  $('#num1').val('');
+  $('#num1').focus();
+  $('#num2').val('');
+  $('#op').val('');
+  $('#result').text('');
+  $('.numbers').val('');
+}
 function calculate(){
   var num1 = $('#num1').val();
   var num2 = $('#num2').val();
-  x= parseFloat(mum1);
+  x= parseFloat(num1);
   y= parseFloat(num2);
   var op = $('#op').val();
+
+  var result= compute(x,y,op);
+  $('#result').text(result)
+
 }
-function compute(){
-  if op ==='+'
-    x+y;
-    break;
-  else if op ==='-'
-    x-y;
-    break;
-  else if op ==='*'
-    x*y;
-    break;
-  else op ==='/'
-    x/y;
-  return result;
+function sum(){
+  var s = 0;
+
+  $('.numbers').each(function(index, element){
+    s += parseFloat(element.value);
+  });
+  $('#result').text(s)
 }
 
-function clear(){
-  
+function prod(){
+  var p = 0;
+
+  $('.numbers').each(function(index,element){
+    p *= parseFloat(element.value)
+  });
+  $('#result').text(p)
+  }
+function compute(x, y, op){
+  var total;
+
+  switch(op){
+    case '+':
+      total = x+y;
+      break;
+    case '-':
+      total = x-y;
+      break;
+    case '*':
+      total =x*y;
+      break;
+    case '/':
+      total =x/y;
+  }
+  return total;
 }
+
