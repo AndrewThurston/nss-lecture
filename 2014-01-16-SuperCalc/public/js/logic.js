97e1f33c6f2a7b632308da8ed9df996ca67b7490
Robert Fryman <robert.fryman@gmail.com>
2014-01-19 15:18:37 -0600

    notes on main.js

--- 2014-01-16-SuperCalc/public/js/logic.js
+++ 2014-01-16-SuperCalc/public/js/logic.js
@@ -1,12 +1,5 @@
-function containsChar(word,letter){
-  if (word.indexOf(letter) >0)
-    return true;
-  else 
-    return false;
-}
-
 function parseTags($tags){
-    return $.map($tags,function(tag){
+  return $.map($tags, function(tag){
     return parseFloat(tag.textContent);
   });
 }
